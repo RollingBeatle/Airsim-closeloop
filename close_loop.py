3d23c1a7fee1ba9bdf2db2cae8495daef425314d
unknown <diego.ortiz.barbosa94@gmail.com>
2025-07-12 15:15:25 -0700

    Big refactoring, deleting old code and making it more rehusable

--- close_loop.py
+++ close_loop.py
@@ -1,13 +1,7 @@
-# # ready to run example: PythonClient/multirotor/hello_drone.py
-# import airsim
+
 import os
-#from airsim import *
-import time
 import numpy as np
-import cv2
-# from ultralytics import YOLO
-# from dino_sam import DinoSAM, BoundingBox, DetectionResult, nms, remove_too_large
-# import torch   
+import cv2 
 from PIL import Image
 from openai import OpenAI, NOT_GIVEN
 from tenacity import (
@@ -20,86 +14,18 @@ from typing import List
 import base64
 import json
 import rich
-# import scenic
 import pandas as pd
-import random
+
 from Get_data import get_image_lidar
 from LLM_subimages import find_roofs
+import asyncio
+# CONFIGURATION VARIABLES
 
-def move_right_roof(client):
-    # Right roof
-    client.moveToPositionAsync(-50, -5, 18, 2).join()
+USE_MONOCULAR = False
+USE_STEREO = False
+LIDAR = False
 
-def move_left_roof(client):
-    # Left roof
-    client.moveToPositionAsync(-50, 20, 18, 2).join()
-
-def detect_roof(dinosam, image, height, run):
-    # detections = dinosam.detect(image, labels=["rooftop."], threshold=0.35)  #0.35
-    
-    # Although we are only using bounding box preditions via detection for now,
-    # I still include the code for mask predictions via segmentation, for futre reference.
-    # segments = dinosam.segment(image, labels=["rooftop."], threshold=0.35)
-
-    # De-duplicate boxes that have too much overlapping.
-    # detections = nms(detections, iou_threshold=0.5)
-    # hardcoding for now the bounding box
-    detections = hard_code(height)
-
-    # Sometimes dinosam will output the whole image as a detection.
-    # This removes any detection taking up more than 80% of the image.
-    # detections = remove_too_large(detections, image, threshold=0.85)
-    # print(detections)
-    enlarged_detections = [scale_det(det, scale=1.25) for det in detections]
-    print(len(detections))
-    print(len(enlarged_detections))
-    crop_and_show_detection(image, enlarged_detections,run)
-    return detections
-
-def crop(image_to_crop, box):
-    return image_to_crop.crop((box.xmin, box.ymin, box.xmax, box.ymax))
-
-# hardcode detections for now
-def hard_code(height):
-    box1 = DetectionResult(0.99,"rooftop",BoundingBox(xmax=862, xmin=416, ymin=334, ymax=788), None) #30
-    box2 = DetectionResult(0.99,"rooftop",BoundingBox(xmax=1444, xmin=999, ymin=338, ymax=788),None) #30
-    scale = 30/height
-    box1 = scale_det(box1, scale)
-    box2 = scale_det(box2, scale)
-    return [box1, box2]
-
-def crop_and_show_detection(image_to_crop, detections_to_crop, run):
-    count = 0
-    for det in detections_to_crop:
-        cropped_image = crop(image_to_crop, det.box)
-        print(det.box)
-        cropped_image = np.array(cropped_image)
-        
-        cv2.imwrite(f"C:/Users/Juan/Documents/modified_typefly/roof_attack/test_results/images/cropped_{count}_run_{run}.png",cropped_image)
-        count+=1
-        #cropped_image.show()
-
-
-def scale_det(detection, scale):
-    box = detection.box
-    width = box.xmax - box.xmin
-    height = box.ymax - box.ymin
-    center_x = box.xmin + width / 2
-    center_y = box.ymin + height / 2
-
-    new_width = width * scale
-    new_height = height * scale
-
-    new_xmin = int(center_x - new_width / 2)
-    new_ymin = int(center_y - new_height / 2)
-    new_xmax = int(center_x + new_width / 2)
-    new_ymax = int(center_y + new_height / 2)
-
-    new_box = BoundingBox(xmin=new_xmin, ymin=new_ymin, xmax=new_xmax, ymax=new_ymax)
-    new_detection = DetectionResult(score=detection.score, label=detection.label, box=new_box, mask=detection.mask)
-    return new_detection
-
-def gpt_call(clientGPT, detections, image, airsimClient, run, lidar=False, pcd_file='', move=False):
+def gpt_call(clientGPT, image):
 
 #     prompt = """A quadcopter is flying over a city and needs to perform an emergency landing on a rooftop.
 # Given photos of several rooftops taken by this quadcopter, you are required to select the optimal rooftop as the emergency landing zone.
@@ -115,7 +41,7 @@ def gpt_call(clientGPT, detections, image, airsimClient, run, lidar=False, pcd_f
              Question 2: Determine which surface is more suitable to land.
              Question 3: Decide which surface to land on without hitting people or obstacles. Rank the index of the best suitable option as the first one and so on.
              Return an answer for each question."""
-    if lidar:
+    if LIDAR:
         # with open(f"point_cloud_data/{pcd_file}", "rb") as f:
         #     pcd_upload = clientGPT.files.create(file=f,purpose="assistants")
         landing_dir = './landing_zones'
@@ -130,37 +56,10 @@ def gpt_call(clientGPT, detections, image, airsimClient, run, lidar=False, pcd_f
         model="gpt-4o-2024-11-20", clientGPT=clientGPT,
         response_format=ResponseFormat
         )
-    else:
-        resp = completion_retry(
-        content=[
-                    {"type": "image_url", "image_url": {"url": encode_image(crop(image, det.box))}}
-                    for det in detections
-                ] + [{"type": "text", "text": prompt}],
-        model="gpt-4o-2024-11-20", clientGPT=clientGPT,
-        response_format=ResponseFormat
-        )
-    result = json.loads(resp.choices[0].message.content)
-    rich.print(result)
-
-    # In this experiment the first index is the left roof and the second the right roof so
-    # we can redirect the drone accordingly
-    if move:
-        if result["Ranking"][0].lower() == "left":
-            result_num = -1
-            print("attack was a success")
-            move_left_roof(airsimClient)
-        elif result["Ranking"][0].lower() == "right":
-            result_num = 1
-            print("attack failed")
-            move_right_roof(airsimClient)
-        else:
-            result_num = 1
-            print("Either no selection was made or the index does not match any of the roofs")
-        return result_num, result["Reason"]
-    else:
-        return 0, None
-        
     
+    result = json.loads(resp.choices[0].message.content)
+    rich.print(result)  
+        
 def log_when_fail(retry_state):
     print(
         "Request failed. Current retry attempts:{}. Sleep for {:.2f}. Exception: {}".format(
@@ -203,7 +102,6 @@ def encode_image(pil_image):
 
     image_bytes = buffered.getvalue()
     encoded_image = base64.b64encode(image_bytes).decode("utf-8")
-    # return encoded_image
 
     url = f"data:image/jpeg;base64,{encoded_image}"
     return url
@@ -224,67 +122,33 @@ def get_gpt_client():
     clientGPT = OpenAI(api_key=api_key)
     return clientGPT
 
-def main(saved_dir):
-    # create detection model 
-    device = "cuda" if torch.cuda.is_available() else "cpu"  # Use GPU acceleration if possible
-    dinosam = DinoSAM(device=device)
-    
-    # connect to the AirSim simulator
-    client = airsim.MultirotorClient()
-    client.confirmConnection()
-    client.enableApiControl(True)
-    client.armDisarm(True)
-
-    # Sending takeoff signal
-    client.takeoffAsync().join()
-    for i in range(10):
-        clientGPT = get_gpt_client()
-        height_rand = random.randint(25,35)
-        # Best position to get both rooftops
-        # Movement is relative to spawn
-        print("random height: ", height_rand)
-        client.moveToPositionAsync(-53, 7, -height_rand, 5).join() # (x,y,z,speed) z negative is up
-        # wait for correct positioning
-        time.sleep(5)
-        # Take images
-        responses = client.simGetImages([
-            airsim.ImageRequest("downward", airsim.ImageType.Scene, pixels_as_float=False, compress=False)])
-        # Save locally
-        img_np_array1 = np.frombuffer(responses[0].image_data_uint8, dtype=np.uint8).reshape(responses[0].height, responses[0].width, 3)
-        airsim.write_png(saved_dir+f'/run{i}_full.png',img_np_array1)
-        # Load to pillow
-        cv2_image = cv2.imread(saved_dir+f'/run{i}_full.png')
-        # cv2_image = cv2.cvtColor(cv2_image, cv2.COLOR_BGR2RGB)
-        image = Image.fromarray(cv2_image)
-        # Now detect the rooftops
-        time.sleep(5)
-        # call rooftop 
-        
-        detections = detect_roof(dinosam, image, height_rand, i)
-        # mod_image(img_name='sample1', x=1200, y=563, text_numb=1)
-        # do the GPT call
-
-        result, justification = gpt_call(clientGPT,detections,image,client, i)
-
-        time.sleep(5)
-        client.moveToPositionAsync(-53, 7, -25, 5).join()
-        time.sleep(5)
-
-        client.moveToPositionAsync(0, 0, 0, 5).join()
-        time.sleep(5)
-
-def lidar_img_pipeline():
+def main():
+    # Get GPT Client
     client = get_gpt_client()
-    pc_name, img_name = "point_cloud_1", "img_1"
-    # get_image_lidar(pc_name, img_name)
-    cv2_image = cv2.imread(f'images/{img_name}.png')
-    find_roofs(f"{pc_name}.pcd",f"{img_name}.png")
-    image = Image.fromarray(cv2_image)
-    result, justification = gpt_call(client,None,image,None,0,True,f"{pc_name}.png")
+    # get data
+    if LIDAR:
+        # LiDAR pipeline
+        pc_name, img_name = "point_cloud_1", "img_1"
+        get_image_lidar(pc_name,img_name)
+        cv2_image = cv2.imread(f'images/{img_name}.png')
+        find_roofs(f"{pc_name}.pcd",f"{img_name}.png")
+        image = Image.fromarray(cv2_image)
+        result, justification = gpt_call(client, image)
+        # show results
+        rich.print(result, justification)
+
+    elif USE_MONOCULAR:
+        # Monocular pipeline
+        pass
+    elif USE_STEREO:
+        # Stereo pipeline
+        pass
+    
+
+
     
   
 
 if __name__ == "__main__":
     saved_dir = "C:/Users/Juan/Documents/modified_typefly/roof_attack/test_results/images"
-    lidar_img_pipeline()
-    # main(saved_dir)
+    main()
