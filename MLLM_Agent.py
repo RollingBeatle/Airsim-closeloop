71eb11d15187b080e5868ddde255585a7ef7f518
unknown <diego.ortiz.barbosa94@gmail.com>
2025-08-06 16:41:54 -0700

    Separated MLLM code from main closeloop - New MLLM agent interface
    - Specific implementation for GPT

---
+++ MLLM_Agent.py
@@ -0,0 +1,170 @@
+"""
+MLLM Agent for Autonomous Drone Recovery
+
+Author: Diego Ortiz Barbosa
+August 2025
+"""
+from abc import ABC, abstractmethod
+import os
+import numpy as np
+import cv2 
+from PIL import Image
+from openai import OpenAI, NOT_GIVEN
+from tenacity import (
+    retry,
+    wait_random_exponential,
+)
+from io import BytesIO
+from pydantic import BaseModel
+from typing import List
+import base64
+import json
+import rich
+
+class MLLMAgent(ABC):
+
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def get_mllm_agent(self):
+        pass
+    @abstractmethod
+    def mllm_call(self):
+        pass
+    
+    @abstractmethod
+    def format_image(self):
+        pass
+
+    @abstractmethod
+    def log_errors(self):
+        pass
+
+class ResponseFormatBasic(BaseModel):
+    Answer: List[str]
+    Indices: List[str]
+
+
+class GPTAgent(MLLMAgent):
+
+    USE_MONOCULAR = True
+    USE_STEREO = False
+    LIDAR = False
+    MOVE = True
+    PROMPTS_FILE = 'prompts.json'
+
+    def get_mllm_agent(self):
+
+        with open("my-k-api.txt", "r") as f:
+            api_key = f.read().strip()
+
+        clientGPT = OpenAI(api_key=api_key)
+        self.client = clientGPT
+        return clientGPT
+    
+    def mllm_call(self, image, distance):
+        # Get GPT Client
+        clientGPT = self.get_mllm_agent()
+        with open(self.PROMPTS_FILE, 'r') as f:
+                # Parsing the JSON file into a Python dictionary
+                prompts = json.load(f)
+
+        if self.LIDAR:
+            prompt = prompts["grid_prompt"]
+        
+        elif self.USE_MONOCULAR or self.USE_STEREO:
+            prompt = prompts["basic_prompt"]
+            
+        landing_dir = './landing_zones'
+        
+        detections = [Image.fromarray(cv2.imread(os.path.join(landing_dir, f)))
+                        for f in os.listdir(landing_dir)]
+        if not detections or distance < 20 :
+            # Crop the original image
+            print("there are no detections, either detector failed or the whole surface is the detection\n We assume the second one")
+            detection = Image.fromarray(cv2.imread(image))
+            w, h = detection.size
+            upper_left = detection.crop((0,0,w//2,h//2))
+            upper_right = detection.crop((w//2,0,w,h//2))
+            bottom_left = detection.crop((0,h//2,w//2,h//2))
+            bottom_right = detection.crop((w//2,h//2,w,h))
+
+            left = (w - 150) // 2
+            top = (h - 150) // 2
+            right = (w + 150) // 2
+            bottom = (h + 150) // 2
+            center = detection.crop((left,top,right,bottom))
+
+            detections = [upper_left, upper_right, bottom_left, bottom_right, center]
+        # we want to send at most 4 areas to the LLM
+        if len(detections) > 4:
+            sorted_images_by_area = sorted(detections, key=lambda img: img.width * img.height)
+            detections = sorted_images_by_area[:4]
+        
+        
+        resp = self.completion_retry(
+        content=[
+                    {"type": "image_url", "image_url": {"url": self.format_image(det)}}
+                    for det in detections  
+                ] + [{"type": "text", "text": prompt}],
+        model="gpt-4o-2024-11-20", clientGPT=clientGPT,
+        response_format=ResponseFormatBasic
+        )
+
+        result = json.loads(resp.choices[0].message.content)
+        rich.print(result)  
+        # detections[int(result['Indices'][0])].show()
+        return detections[int(result['Indices'][0])]
+        # return result['Coordinates'][0]
+    
+    def log_errors(self, retry_state):
+        print(
+        "Request failed. Current retry attempts:{}. Sleep for {:.2f}. Exception: {}".format(
+            retry_state.attempt_number, retry_state.idle_for, repr(retry_state.outcome.exception())
+        )
+    )
+    
+    @retry(
+    wait=wait_random_exponential(min=1, max=60),
+    before_sleep=log_errors)
+    def completion_retry(self, content, model, clientGPT, response_format=NOT_GIVEN):
+        response = clientGPT.beta.chat.completions.parse(
+            model=model,
+            messages=[
+                {
+                    "role": "user",
+                    "content": content
+                }
+            ],
+            max_tokens=2000,
+            temperature=0,
+            response_format=response_format
+        )
+
+        if response.choices[0].finish_reason != "stop":
+            raise ValueError(
+                "Generation finish reason: {}. {}".format(
+                    response.choices[0].finish_reason,
+                    response.usage.to_dict()
+                )
+            )
+
+        return response
+    
+    def format_image(self, pil_image):
+        buffered = BytesIO()
+        pil_image.save(buffered, format="PNG")
+
+        image_bytes = buffered.getvalue()
+        encoded_image = base64.b64encode(image_bytes).decode("utf-8")
+
+        url = f"data:image/jpeg;base64,{encoded_image}"
+        return url
+
+
+
+
+    
+        
+    
--- close_loop.py
+++ close_loop.py
@@ -20,10 +20,10 @@ import shutil
 from LiDAR.Get_data import get_image_lidar
 from LiDAR.LLM_subimages import find_roofs
 from drone_movement import monocular_landing, stereo_landing
-
+from MLLM_Agent import GPTAgent
 
 # CONFIGURATION VARIABLES
-
+# TODO: integrate configuration into agent
 USE_MONOCULAR = True
 USE_STEREO = False
 LIDAR = False
@@ -32,133 +32,6 @@ PROMPTS_FILE = 'prompts.json'
 DELETE_LZ = True
 DIRS = ["images", "landing_zones","point_cloud_data"]
 
-def gpt_call(image) -> Image:
-
-#     prompt = """A quadcopter is flying over a city and needs to perform an emergency landing on a rooftop.
-# Given photos of several rooftops taken by this quadcopter, you are required to select the optimal rooftop as the emergency landing zone.
-
-# Avoid any obstructions or people on the rooftop.
-
-# First, explain your reasoning behind the rooftop ranking in detail, including the analysis for each rooftop. \
-# Then, output the indices corresponding to each photo, representing the ranking from the most suitable rooftop to the least one.
-# The index starts at 0.
-# """
-    # Get GPT Client
-    clientGPT = get_gpt_client()
-    with open(PROMPTS_FILE, 'r') as f:
-            # Parsing the JSON file into a Python dictionary
-            prompts = json.load(f)
-
-    if LIDAR:
-        prompt = prompts["grid_prompt"]
-    
-    elif USE_MONOCULAR or USE_STEREO:
-        prompt = prompts["basic_prompt"]
-        
-    landing_dir = './landing_zones'
-    try:
-        detections = [Image.fromarray(cv2.imread(os.path.join(landing_dir, f)))
-                      for f in os.listdir(landing_dir)]
-    except:
-        # Crop the original image
-        print("there are no detections, either detector failed or the whole surface is the detection\n We assume the second one")
-        detection = Image.fromarray(cv2.imread(image))
-        w, h = detection.size
-        upper_left = detection.crop((0,0,w//2,h//2))
-        upper_right = detection.crop((w//2,0,w,h//2))
-        bottom_left = detection.crop((0,h//2,w//2,h//2))
-        bottom_right = detection.crop((w//2,h//2,w,h))
-
-        left = (w - 150) // 2
-        top = (h - 150) // 2
-        right = (w + 150) // 2
-        bottom = (h + 150) // 2
-        center = detection.crop((left,top,right,bottom))
-
-        detections = [upper_left, upper_right, bottom_left, bottom_right, center]
-    # we want to send at most 4 areas to the LLM
-    if len(detections) > 4:
-        sorted_images_by_area = sorted(detections, key=lambda img: img.width * img.height)
-        detections = sorted_images_by_area[:4]
-    
-    
-    resp = completion_retry(
-    content=[
-                {"type": "image_url", "image_url": {"url": encode_image(det)}}
-                for det in detections  
-            ] + [{"type": "text", "text": prompt}],
-    model="gpt-4o-2024-11-20", clientGPT=clientGPT,
-    response_format=ResponseFormatBasic
-    )
-
-    result = json.loads(resp.choices[0].message.content)
-    rich.print(result)  
-    detections[int(result['Surfaces'][0])-1].show()
-    return detections[int(result['Surfaces'][0])-1]
-    # return result['Coordinates'][0]
-        
-def log_when_fail(retry_state):
-    print(
-        "Request failed. Current retry attempts:{}. Sleep for {:.2f}. Exception: {}".format(
-            retry_state.attempt_number, retry_state.idle_for, repr(retry_state.outcome.exception())
-        )
-    )
-
-
-@retry(
-    wait=wait_random_exponential(min=1, max=60),
-    before_sleep=log_when_fail
-)
-def completion_retry(content, model, clientGPT, response_format=NOT_GIVEN):
-    response = clientGPT.beta.chat.completions.parse(
-        model=model,
-        messages=[
-            {
-                "role": "user",
-                "content": content
-            }
-        ],
-        max_tokens=2000,
-        temperature=0,
-        response_format=response_format
-    )
-
-    if response.choices[0].finish_reason != "stop":
-        raise ValueError(
-            "Generation finish reason: {}. {}".format(
-                response.choices[0].finish_reason,
-                response.usage.to_dict()
-            )
-        )
-
-    return response
-
-def encode_image(pil_image):
-    buffered = BytesIO()
-    pil_image.save(buffered, format="PNG")
-
-    image_bytes = buffered.getvalue()
-    encoded_image = base64.b64encode(image_bytes).decode("utf-8")
-
-    url = f"data:image/jpeg;base64,{encoded_image}"
-    return url
-
-
-# Specify the response format for GPT to make sure its output is structured as follows
-class ResponseFormat(BaseModel):
-    Answers: List[str]
-    Coordinates: List[str]
-class ResponseFormatBasic(BaseModel):
-    Answer: List[str]
-    Surfaces: List[str]
-
-def get_gpt_client():
-    # read the API key and create the GPT client
-    with open("my-k-api.txt", "r") as f:
-        api_key = f.read().strip()
-
-    clientGPT = OpenAI(api_key=api_key)
-    return clientGPT
 
 def create_subdirs():
     # add more dirs if needed
@@ -179,6 +52,7 @@ def clear_dirs():
 
     
 def main():
+    MLLM_Agent = GPTAgent()
     # get data
     if DELETE_LZ: clear_dirs()
     create_subdirs()
@@ -189,16 +63,16 @@ def main():
         cv2_image = cv2.imread(f'images/{img_name}.png')
         find_roofs(f"{pc_name}.pcd",f"{img_name}.png")
         image = Image.fromarray(cv2_image)
-        result, justification = gpt_call(image)
+        result, justification = MLLM_Agent.mllm_call(image)
         # show results
         rich.print(result, justification)
 
     elif USE_MONOCULAR:
         # Monocular pipeline
-        monocular_landing(gpt_call,MOVE)
+        monocular_landing(MLLM_Agent.mllm_call,MOVE)
     elif USE_STEREO:
         # Stereo pipeline
-        stereo_landing(gpt_call,MOVE)
+        stereo_landing(MLLM_Agent.mllm_call,MOVE)
     
 
 if __name__ == "__main__":
